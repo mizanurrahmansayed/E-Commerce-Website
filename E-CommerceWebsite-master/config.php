1f85a4ebb9666ef70a9d7c50e1d6555b15a63b1b
mizanurrahmansayed <mizanur.rahmen.sayed@gmail.com>
2025-05-19 01:38:24 +0800

    add my website all file

---
+++ E-CommerceWebsite-master/config.php
@@ -0,0 +1,4 @@
+$servername = "localhost";
+$username = "root"; // Default MySQL username for XAMPP
+$password = ""; // Default MySQL password for XAMPP (leave empty)
+$dbname = "your_database_name"; // Replace with your actual database name
