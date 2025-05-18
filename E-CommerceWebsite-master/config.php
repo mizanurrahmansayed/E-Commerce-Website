54721cffb868a9bf85b4c1708ed5912439b109f0
mizanurrahmansayed <mizanur.rahmen.sayed@gmail.com>
2025-05-19 03:51:07 +0800

    remove one file

--- E-CommerceWebsite-master/config.php
+++
@@ -1,4 +0,0 @@
-$servername = "localhost";
-$username = "root"; // Default MySQL username for XAMPP
-$password = ""; // Default MySQL password for XAMPP (leave empty)
-$dbname = "your_database_name"; // Replace with your actual database name
